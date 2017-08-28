48bb3653c003cbf1556934f830f5a179d5f37620
TiffyB <tiffanyabarth@gmail.com>
2017-08-28 16:47:51 -0700

    Finished BeesBeesBees

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,11 @@
 var Bee = function() {
+	Grub.call(this);
+	//this = Object.create(Grub.prototype);
+	this.age = 5;
+	this.color = "yellow";
+	this.job = "keep on growing";
 };
+
+Bee.prototype = Object.create(Grub.prototype);
+Bee.prototype.constructor = Bee;
 
